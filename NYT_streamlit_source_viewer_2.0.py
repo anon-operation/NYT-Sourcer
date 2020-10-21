40424fff786098387fbf206cd9bbb928a6ca2993
anon-operation <cksegarra@gmail.com>
2020-10-21 18:50:37 -0400

    Functional update

--- NYT_streamlit_source_viewer_2.0.py
+++ NYT_streamlit_source_viewer_2.0.py
@@ -26,11 +26,6 @@ class StoryMetadata:
 
 
 
-
-filename="runningDatabase"
-totalFileName=(filename+".pickle")
-outfile = open("streamlitData.csv", "w", encoding='utf-8')
-
 sqlfilename = "NYT.sqlite"
 
 connex = sqlite3.connect(sqlfilename) #establish a connection
@@ -63,38 +58,6 @@ def runPandaQuery (fieldName,queryFieldName,queryValue):
 
 
 
-
-##@st.cache # Cache data after loading
-##def load_NYT_data():
-##    with open(totalFileName,'rb') as handle:
-##        unserialized_data = pickle.load(handle)
-##    nameTallylist = []
-##    print("Name", "Section", "Url", "Date", "Title", "Count",sep =",",file=outfile)
-##    for key in unserialized_data:
-##        try:
-##            article=unserialized_data[key]
-##            title=key.replace(",", " ")
-##            #title=key
-##            section=article.section
-##            url=article.url
-##            author=article.author
-##            date=str(article.pubDate)
-##            fixedDate=date[:-6]
-##            count=1
-##            innerList=[section,url,author,date]
-##            sources=article.sources
-##            NLTKs=sources['NLTK']
-##            for name in NLTKs:
-##                lower=name.lower()
-##                print(lower, section, url, fixedDate, title, count, sep=",",file=outfile)
-##        except:
-##            tryVar=0
-##    outfile.flush()
-##    outfile.close()
-##
-##    df=pd.read_csv('streamlitData.csv', sep=',')
-##
-##    return df
 
 
 def highlight_name1(val):
@@ -153,21 +116,23 @@ if modifyChart:
                                         "Opinion", "N.Y. Region", "World",
                                         "Climate", "Dining", "Technology",
                                         "Style", "Sports", "Arts"))
+        attribute_selectbox=st.selectbox("Type of attribute", ("All","NLTK", "High", "Low", "Known"))
+        if attribute_selectbox == "All":
+            attributeChoice='%'
+        else:
+            attributeChoice=attribute_selectbox.lower()
         startDate=st.date_input("Start date", pd.to_datetime("2020-09-29"))
         qDate1 = str((str(startDate))+"T00:00:00")
-        #theStart=pd.to_datetime(str(startDate))
         endDate=st.date_input("End date", pd.to_datetime("2020-10-10"))
         qDate2 = str((str(endDate))+"T24:59:59")
-        #theEnd=pd.to_datetime(str(endDate))
         if startDate <= endDate:
-            timeQueryString = " BETWEEN "+"'"+qDate1+"'"+" AND "+"'"+qDate2+"'"+" ;"
-            data=runPandaQuery ("*","Date",timeQueryString)  ##(fieldName,queryFieldName,queryValue)  ("*","Section"," LIKE 'healt%';")
-            df=pd.DataFrame(data)
-            df['Date']=pd.to_datetime(df['Date'], format='%Y-%m-%dT%H:%M:%S')
-            df['Publication Date']=df['Date']
             if section_selectbox=="All sections":
-                st.text("Chart shows names that appear more than once.")
-                df3=df.loc[df.duplicated(subset='Name', keep=False), :]
+                fullQueryString = " BETWEEN "+"'"+qDate1+"'"+" AND "+"'"+qDate2+"'"+" AND Type LIKE  "+"'"+attributeChoice+"'"+" ;"
+                data=runPandaQuery ("*","Date",fullQueryString)  ##(fieldName,queryFieldName,queryValue)  ("*","Section"," LIKE 'healt%';")
+                df=pd.DataFrame(data)
+                df['Date']=pd.to_datetime(df['Date'], format='%Y-%m-%dT%H:%M:%S')
+                df['Publication Date']=df['Date']
+                df3=df
                 fig=px.bar(df3, x="Name", y="Count", color="Section",
                 hover_data=["Title", "Url", "Publication Date"])
                 st.plotly_chart(fig, use_container_width=True)
@@ -175,16 +140,18 @@ if modifyChart:
                 try:
                     lowercaseSelection=section_selectbox.lower()
                     if lowercaseSelection=="u.s.":
-                        df3=df[df['Section'].str.contains("us")]
-                        df3=df3[~df3['Section'].str.contains("ness")]
-                        fig=px.bar(df3, x="Name", y="Count", color="Section",
-                            hover_data=["Title", "Url", "Date"])
-                        st.plotly_chart(fig, use_container_width=True)
-                    else:
-                        df3=df[df['Section'].str.contains(lowercaseSelection)]
-                        fig=px.bar(df3, x="Name", y="Count", color="Section",
-                                hover_data=["Title", "Url", "Date"])
-                        st.plotly_chart(fig, use_container_width=True)
+                        lowercaseSelection="us"
+                    elif lowercaseSelection=="n.y. region":
+                        lowercaseSelection="nyregion"
+                    fullQueryString = " BETWEEN "+"'"+qDate1+"'"+" AND "+"'"+qDate2+"'"+" AND Type LIKE  "+"'"+attributeChoice+"'"+" AND Section LIKE "+"'"+lowercaseSelection+"'"+" ;"
+                    data=runPandaQuery ("*","Date",fullQueryString)  ##(fieldName,queryFieldName,queryValue)  ("*","Section"," LIKE 'healt%';")
+                    df=pd.DataFrame(data)
+                    df['Date']=pd.to_datetime(df['Date'], format='%Y-%m-%dT%H:%M:%S')
+                    df['Publication Date']=df['Date']
+                    df3=df
+                    fig=px.bar(df3, x="Name", y="Count", color="Section",
+                    hover_data=["Title", "Url", "Publication Date", "Name"])
+                    st.plotly_chart(fig, use_container_width=True)
                 except:
                     st.error("Error: The database has no stories for the given dates and section. Try a wider date range or click All sections")
         else:
@@ -243,7 +210,11 @@ st.text("")
 st.subheader("Search for a specific article")
 article_user_input = st.text_input("Search for an article.", "")
 lowerArtName=article_user_input.lower()
-articleDf=df
+
+data=runPandaQuery ("*","Count"," ==1 ;")  ##(fieldName,queryFieldName,queryValue)  ("*","Section"," LIKE 'healt%';") 
+articleDf=pd.DataFrame(data)
+articleDf['Date']=pd.to_datetime(articleDf['Date'], format='%Y-%m-%dT%H:%M:%S')
+articleDf['Publication Date']=articleDf['Date']
 articleDf['Title']=articleDf['Title'].str.lower()
 articleDf.loc[:,'Title']=articleDf.loc[:,'Title'].str.replace("`|’", "'", regex=True)
 artIndexList=articleDf[articleDf['Title'].str.contains(lowerArtName)]
@@ -254,64 +225,10 @@ else:
     printList=st.button("Get list of names in aticle(s) with \""+article_user_input+"\" in the title")
     if printList:
         listDf=artIndexList['Name']
-        st.text("You can copy this list of names.")
-        st.dataframe(listDf)
+        st.text("You can highlight and copy this list of names:")
+        for name in listDf:
+            st.text(name)
+
  
 
 
-
-
-##
-##DATE_COLUMN='date/time'
-##DATA_URL=('https://s3-us-west-2.amazonaws.com/'
-##          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-##
-##@st.cache # Cache data after loading
-##def load_data(nrows):
-##    data=pd.read_csv(DATA_URL, nrows=nrows)
-##    lowercase = lambda x: str(x).lower()
-##    data.rename(lowercase, axis='columns', inplace=True)
-##    data[DATE_COLUMN]=pd.to_datetime(data[DATE_COLUMN])
-##    return data
-##
-### create loading message
-##data_load_state=st.text('Loading data......')
-##
-### Load 10k rows of data
-##data=load_data(10000)
-##
-### Successful load message
-##data_load_state.text('Data load done!...and it\'s cached!..and saves automatically.')
-##
-##
-### view data with button option
-##if st.checkbox("Show the raw data"):
-##    st.subheader('Raw Data')
-##    st.write(data) #should select correct type of render automatically. If not, it can be forced to a specific type (see API)
-##
-##
-### make graph
-##st.subheader('Pickups per hour')
-##hist_values= np.histogram(
-##    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-##st.bar_chart(hist_values)
-##
-##
-### make a map
-##st.subheader("Map of pickups")
-##st.map(data)
-##
-### make a better map
-##hour_to_filter=17
-##filtered_data=data[data[DATE_COLUMN].dt.hour==hour_to_filter]
-##st.subheader(f'Map of all pickups at {hour_to_filter}:00')
-##st.map(filtered_data)
-##
-### add a slider
-##choose_hour_to_filter=st.slider("hour", 0, 23, 17) # minimum: 0h, maximum: 23h, default = 17h
-##filtered_data=data[data[DATE_COLUMN].dt.hour==choose_hour_to_filter]
-##st.subheader(f'Map of all pickups at {hour_to_filter}:00')
-##st.map(filtered_data)
-##
-##
-##
